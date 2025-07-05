e21714dc3509120188d150331747d44ce5796027
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2025-07-06 03:33:58 +0800

    Feature preprocessing and denormalization dummy functions added

--- model/forecast.py
+++ model/forecast.py
@@ -1,3 +1,37 @@
+def forecast_model_dummy(muni_data: dict, weather_api_data: dict) -> list:
+    forecast_list = []
+
+    if (weather_api_data.get("daily") and
+        weather_api_data["daily"].get("precipitation_sum") and
+        weather_api_data["daily"].get("time")):
+
+        preprocessed_data = preprocessing_data(weather_api_data)
+
+        num_days = len(preprocessed_data["daily"]["precipitation_sum"])
+        for j in range(num_days):
+            forecast_list.append({
+                "date": preprocessed_data["daily"]["time"][j],
+                "rain": round(preprocessed_data["daily"]["precipitation_sum"][j]),
+                "weathercode": preprocessed_data["daily"]["weathercode"][j] if preprocessed_data["daily"].get("weathercode") else None,
+                "icon": get_weather_icon_from_code(preprocessed_data["daily"]["weathercode"][j] if weather_api_data["daily"].get("weathercode") else None)
+            })
+
+    return denormalization(forecast_list)
+
+# Preparation of input data for trained model
+def preprocessing_data(weather_api_data: dict) -> list:
+
+    # Requires the trained imputation model and normalization scaler
+
+    return weather_api_data
+
+# Function using Min-Max scaler to get original values
+def denormalization(forecast_list: dict) -> list:
+    
+    # Requires the trained normalization scaler
+
+    return forecast_list
+
 # Helper function to get weather icon based on weather code
 def get_weather_icon_from_code(weathercode: int):
     """Returns an emoji icon based on the OpenMeteo weathercode."""
@@ -8,19 +42,3 @@ def get_weather_icon_from_code(weathercode: int):
     if weathercode >= 51: return '🌦️'
     if weathercode >= 1: return '🌤️'
     return '☀️'
-
-def forecast_model_dummy(muni_data: dict, weather_api_data: dict) -> list:
-    forecast_list = []
-    if (weather_api_data.get("daily") and
-        weather_api_data["daily"].get("precipitation_sum") and
-        weather_api_data["daily"].get("time")):
-
-        num_days = len(weather_api_data["daily"]["precipitation_sum"])
-        for j in range(num_days):
-            forecast_list.append({
-                "date": weather_api_data["daily"]["time"][j],
-                "rain": round(weather_api_data["daily"]["precipitation_sum"][j]),
-                "weathercode": weather_api_data["daily"]["weathercode"][j] if weather_api_data["daily"].get("weathercode") else None,
-                "icon": get_weather_icon_from_code(weather_api_data["daily"]["weathercode"][j] if weather_api_data["daily"].get("weathercode") else None)
-            })
-    return forecast_list
